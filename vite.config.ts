77b165285cf455ece22eab8bc4799c13e41b2d3e
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-10-28 03:34:44 +0100

    chore: try esm (#56)

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     minify: false,
-    target: "node20",
+    target: "node24",
     ssr: true,
     rollupOptions: {
       input: ["src/index.ts"],
       output: {
-        entryFileNames: "index.cjs",
-        format: "cjs",
+        entryFileNames: "index.mjs",
+        format: "esm",
         exports: "auto",
       },
     },
